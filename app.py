5c91b3ae7dc20b1a40df41c2a6bd2aaf392fe910
Saivardhan-123 <saivardhansaineni@gmail.com>
2026-06-21 20:36:55 +0530

    changes done for deploy

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 ##Load the model
@@ -34,4 +35,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port, debug=False)
